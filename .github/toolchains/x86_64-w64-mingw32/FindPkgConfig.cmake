056cee8585bb1eb5294a45076a1d70b29892e0b4
Matthias Schiffer <mschiffer@universe-factory.net>
2021-12-21 23:35:00 +0100

    build: replace FindPkgConfig hack with BUILD_STATIC variable set by CI toolchain file

--- .github/toolchains/x86_64-w64-mingw32/FindPkgConfig.cmake
+++
@@ -1,5 +0,0 @@
-# Hack: The toolchain file is evaluated too early to set the library suffixes, so we use a wrapper
-# around FindPkgConfig set it right before we search for libraries.
-set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-
-include(${CMAKE_ROOT}/Modules/FindPkgConfig.cmake)
