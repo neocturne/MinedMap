4b83f6376f39b34e61e291faee873a7cb444b72b
Matthias Schiffer <mschiffer@universe-factory.net>
2023-01-24 18:46:48 +0100

    Remove C++ implementation

--- .github/toolchains/i686-w64-mingw32.cmake
+++
@@ -1,2 +0,0 @@
-set(TARGET i686-w64-mingw32)
-include(${CMAKE_CURRENT_LIST_DIR}/common-w64-mingw32.cmake)
--- .github/toolchains/x86_64-w64-mingw32.cmake
+++
@@ -1,2 +0,0 @@
-set(TARGET x86_64-w64-mingw32)
-include(${CMAKE_CURRENT_LIST_DIR}/common-w64-mingw32.cmake)
